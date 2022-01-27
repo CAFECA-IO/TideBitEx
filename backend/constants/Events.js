d7305b073bf1c5fa08a42031a0a088162490a026
Waiting4Ever <junsr.tw@hotmail.com>
2022-01-27 11:56:27 +0800

    #38 add event emitter

---
+++ backend/constants/Events.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // subscribe pair event
+  pairOnSubscribe: 'pairOnSubscribe',
+
+  // update event
+  pairOnUpdate: 'pairOnUpdate',
+  tradeDataOnUpdate: 'tradeDataOnUpdate',
+  orderOnUpdate: 'orderOnUpdate',
+  candleOnUpdate: 'candleOnUpdate',
+}
