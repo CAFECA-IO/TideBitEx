93be9147834fec18f68a8ec52f0e8309111f941c
Luphia Chang <luphia@mermer.cc>
2022-02-10 10:58:18 +0800

    Update TradingChart.js

--- src/components/TradingChart.js
+++ src/components/TradingChart.js
@@ -7,6 +7,7 @@ import ApexCharts from "react-apexcharts";
 const TradingChart = (props) => {
   const storeCtx = useContext(StoreContext);
   // candleBarHandler ++TODO
+
   return (
     <div className="main-chart mb15">
       {/* {storeCtx.priceData && ( */}
@@ -200,9 +201,23 @@ const TradingChart = (props) => {
               },
             }}
           />
-        )}
-      </div>
-    </>
+          {/* <TradingViewWidget
+              symbol={`OKEX:${storeCtx.selectedTicker.instId?.replace(
+                "-",
+                ""
+              )}`}
+              theme={props.theme === "light" ? Themes.LIGHT : Themes.DARK}
+              locale="en"
+              autosize
+              interval="D"
+              timezone="America/New_York"
+              library_path="charting_library/"
+              allow_symbol_change={false}
+              hide_legend={true}
+            /> */}
+        </React.Fragment>
+      )}
+    </div>
   );
 };
 
