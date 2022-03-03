42666fb71f8566ba8f95180cf120942163b17d50
Waiting4Ever <junsr.tw@hotmail.com>
2022-03-03 19:05:03 +0800

    add cancel order save into db

--- backend/libs/Utils.js
+++ backend/libs/Utils.js
@@ -622,6 +622,17 @@ class Utils {
   static removeZeroEnd(str) {
     return new BigNumber(str).toFixed();
   }
+
+  static parseClOrdId(clOrdId) {
+    // clOrdId = 377bd372412fSCDE60977m247674466o
+    // brokerId = 377bd372412fSCDE
+    // memberId = 60976
+    // orderId = 247674466
+    const slice1 = clOrdId.slice(16); // slice broker id
+    const split1 = slice1.split('m'); // split memberId
+    const split2 = split1[1].split('o');  // split orderId
+    return split2[0];
+  }
 }
 
 module.exports = Utils;
