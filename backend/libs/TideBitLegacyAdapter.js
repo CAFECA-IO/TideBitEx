5207ee34462ef2d853d492c6bd3ad1c67bb76e40
TzuHanLiang <crocodilestear@gmail.com>
2022-03-15 18:24:40 +0800

    feat: update bodyformdata

--- backend/libs/TideBitLegacyAdapter.js
+++ backend/libs/TideBitLegacyAdapter.js
@@ -1,5 +1,5 @@
-const SafeMath = require('./SafeMath');
-const FormData = require('form-data');
+const SafeMath = require("./SafeMath");
+const FormData = require("form-data");
 
 class TideBitLegacyAdapter {
   constructor({ config, database, logger }) {
@@ -11,24 +11,22 @@ class TideBitLegacyAdapter {
 
   static peatioOrderBody({ header, body }) {
     const bodyFormData = new FormData();
-    bodyFormData.append('utf8', true);
-    console.log('header==============', header);
-    console.log('body-----------', body);
-    bodyFormData.append('authenticity_token', body['X-CSRF-Token']);
+    bodyFormData.append("utf8", "true");
+    bodyFormData.append("authenticity_token", body["X-CSRF-Token"]);
 
-    if (body.side === 'buy') {
-      bodyFormData.append('order_bid[ord_type]', body.ordType);
-      bodyFormData.append('order_bid[origin_volume]', body.sz);
-      if (body.ordType === 'limit') {
-        bodyFormData.append('order_bid[price]', body.px);
-        bodyFormData.append('order_bid[total', SafeMath.mult(body.px, body.sz));
+    if (body.side === "buy") {
+      bodyFormData.append("order_bid[ord_type]", body.ordType);
+      bodyFormData.append("order_bid[origin_volume]", body.sz);
+      if (body.ordType === "limit") {
+        bodyFormData.append("order_bid[price]", body.px);
+        bodyFormData.append("order_bid[total", SafeMath.mult(body.px, body.sz));
       }
-    } else if (body.side === 'sell') {
-      bodyFormData.append('order_ask[ord_type]', body.ordType);
-      bodyFormData.append('order_ask[origin_volume]', body.sz);
-      if (body.ordType === 'limit') {
-        bodyFormData.append('order_ask[price]', body.px);
-        bodyFormData.append('order_ask[total', SafeMath.mult(body.px, body.sz));
+    } else if (body.side === "sell") {
+      bodyFormData.append("order_ask[ord_type]", body.ordType);
+      bodyFormData.append("order_ask[origin_volume]", body.sz);
+      if (body.ordType === "limit") {
+        bodyFormData.append("order_ask[price]", body.px);
+        bodyFormData.append("order_ask[total", SafeMath.mult(body.px, body.sz));
       }
     }
 
