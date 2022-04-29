37483636ee124e9063b01d9dfcd0e541d65c939a
TzuHanLiang <crocodilestear@gmail.com>
2022-04-29 11:31:43 +0800

    feat: uncommon function

--- src/components/TradingIframe.js
+++ src/components/TradingIframe.js
@@ -6,10 +6,11 @@ const TradingIframe = (props) => {
   const [query, setQuery] = useState("");
 
   useEffect(() => {
-    const { name, pricescale } = storeCtx.selectedTicker;
+    const { name, pricescale, source } = storeCtx.selectedTicker;
     const arr = [];
     if (name) arr.push(`symbol=${name}`);
     if (pricescale) arr.push(`pricescale=${pricescale}`);
+    if (source) arr.push(`source=${source}`);
     if (props.isMobile) arr.push(`mobile=${1}`);
     const qs = !!arr.length ? `?${arr.join("&")}` : "";
     setQuery(qs);
