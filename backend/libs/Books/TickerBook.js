64b11ae26fb57d6c0418e7295ced1ffa1a7def68
TzuHanLiang <crocodilestear@gmail.com>
2022-05-16 19:30:30 +0800

    feat: test

--- backend/libs/Books/TickerBook.js
+++ backend/libs/Books/TickerBook.js
@@ -58,6 +58,7 @@ class TickerBook extends BookBase {
         return true;
       }
     } catch (error) {
+      this.logger.error(`[${this.constructor.name}] error`, error);
       return false;
     }
   }
