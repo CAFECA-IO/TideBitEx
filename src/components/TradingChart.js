45382583797fb975bdd2e25ca3c02327340c2f77
TzuHanLiang <crocodilestear@gmail.com>
2022-06-24 15:17:57 +0800

    feat: patch iframe

--- src/components/TradingChart.js
+++ src/components/TradingChart.js
@@ -1,16 +1,16 @@
 import React, { useContext } from "react";
-// import StoreContext from "../store/store-context";
+import StoreContext from "../store/store-context";
 import { useViewport } from "../store/ViewportProvider";
 
-// import TradingApexChart from "./TradingApexChart";
-// import TradingViewChart from "./TradingViewChart";
+import TradingApexChart from "./TradingApexChart";
+import TradingViewChart from "./TradingViewChart";
 import { useTranslation } from "react-i18next";
 import TradingIframe from "./TradingIframe";
 
 const TradingChart = (props) => {
   const { width } = useViewport();
   const breakpoint = 414;
-  // const storeCtx = useContext(StoreContext);
+  const storeCtx = useContext(StoreContext);
   const { t } = useTranslation();
 
   return (
@@ -20,15 +20,13 @@ const TradingChart = (props) => {
       }`}
     >
       <div className="main-chart__header">{t("chart")}</div>
-      {/* {!!storeCtx.selectedTicker && ( */}
-      <TradingIframe isMobile={width <= breakpoint} />
-      {/* )} */}
-      {/* {storeCtx.selectedTicker?.source === "TideBit" ? (
-        // <TradingApexChart />
+      {window.location.host.includes("legacy2") ? (
         <TradingIframe isMobile={width <= breakpoint} />
+      ) : storeCtx.selectedTicker?.source === "TideBit" ? (
+        <TradingApexChart />
       ) : (
         <TradingViewChart theme={props.theme} />
-      )} */}
+      )}
     </div>
   );
 };
