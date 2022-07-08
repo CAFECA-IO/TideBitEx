f05d152f97e8cf182529c2e6c98b3831d1c1fe35
TzuHanLiang <crocodilestear@gmail.com>
2022-07-08 16:31:48 +0800

    feat: test userId

--- src/modal/Middleman.js
+++ src/modal/Middleman.js
@@ -204,7 +204,7 @@ class Middleman {
         this.accountBook.updateAll(accounts);
         const CSRFToken = await this.communicator.CSRFTokenRenew();
         console.log(`[Middleman] _getAccounts userId`, this._userId);
-        const userId = this.userId;
+        const userId = this._userId;
         this.tbWebSocket.setCurrentUser(market, {
           CSRFToken,
           userId,
