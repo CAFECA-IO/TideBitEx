70831580663728646fd5f80a270c33e1fb3e69d3
TzuHanLiang <crocodilestear@gmail.com>
2022-07-08 15:18:29 +0800

    feat: add decodeURIComponent

--- backend/libs/Utils.js
+++ backend/libs/Utils.js
@@ -719,7 +719,7 @@ class Utils {
       return /XSRF-TOKEN/.test(v);
     });
     if (!data) return undefined;
-    const token = data.split("=")[1];
+    const token = decodeURIComponent(data.split("=")[1]);
     return token;
   }
 
